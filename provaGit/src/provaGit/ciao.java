7bd94cdd185c0c69e4be0409295ed5bfea0f2f8a
cavallini.dan.2001 <cavallini.dan.2001@PC228-13.iav.it>
2019-05-14 12:33:13 +0200

    wrfew

--- provaGit/src/provaGit/ciao.java
+++ provaGit/src/provaGit/ciao.java
@@ -4,7 +4,7 @@ public class ciao {
 	public static void main(String [] args) {
 		System.out.println("ciao");
 		System.out.println("ciao");
-		System.out.println("Addio");
+		System.out.println("Ad");
 		System.out.println("pietrocolaSeibrutto");
 	}
 }
