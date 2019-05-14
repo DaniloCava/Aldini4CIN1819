3a6fe0abdaeb1470dbdc38774d4ccdcbe4cbf3fc
pietrocola.and.1999 <pietrocola.and.1999@PC228-15.iav.it>
2019-05-14 12:30:22 +0200

    Caricato <3

--- provaGit/src/provaGit/ciao.java
+++ provaGit/src/provaGit/ciao.java
@@ -3,7 +3,7 @@ package provaGit;
 public class ciao {
 	public static void main(String [] args) {
 		System.out.println("ciao");
-		System.out.println("ciao");
 		System.out.println("Addio");
+		System.out.println("Parbleu");
 	}
 }
